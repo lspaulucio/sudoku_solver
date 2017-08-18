cbcdc2cf442930ac85e2982d5819b5fb083a5e10
Leonardo Santos Paulucio <leonardo.paulucio@hotmail.com>
2017-08-17 22:34:38 -0300

    Solutioner finished, time for tests

--- sudoku.c
+++ sudoku.c
@@ -2,156 +2,161 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void sudoku_read(int v[][9])
-{
-    FILE *sudoku = fopen("sudoku.txt", "r");
-    int i,j;
+#define SUDOKU_SIZE 81
+#define SUDOKU_COL_NUM 9
+#define SUDOKU_LIN_NUM 9
 
-    for(i=0;i<9;i++)
-        for(j=0;j<9;j++)
-            fscanf(sudoku, "%d", &v[i][j]);
+void sudoku_read(int v[SUDOKU_SIZE])
+{
+    FILE *sudoku = fopen("sudoku.txt", "r"); //reading sudoku file
+    int i;
+
+    for(i=0;i<SUDOKU_SIZE;i++)
+        fscanf(sudoku, "%d", &v[i]);
 
     fclose(sudoku);
 }
 
-void sudoku_print(int v[][9])
+void sudoku_print(int v[SUDOKU_SIZE]) //Print sudoku into display
 {
     int i,j;
 
-    for(i=0;i<9;i++)
+    for(i=0;i<SUDOKU_COL_NUM;i++)
     {
-        for(j=0;j<9;j++)
-        // {   if(v[i][j] != 0)
-                printf("%d  ", v[i][j]);
-            // else
-            //     printf("   ");
-        // }
+        for(j=0;j<SUDOKU_COL_NUM;j++)
+            printf("%d ", v[i*SUDOKU_COL_NUM + j]);
 
         printf("\n");
     }
 }
 
-bool sudoku_check(int v[][9], int value, int lin, int col)
+bool sudoku_check(int v[SUDOKU_SIZE], int value, int pos)
 {
     int i, j;
+    int lin, col;
 
-    for(i=0;i<9;i++) //checking the line
-        if(v[lin][i] == value)
+    lin = pos / SUDOKU_COL_NUM; //getting the line number
+    col = pos % SUDOKU_COL_NUM; //getting the column number
+
+    for(i=0; i<SUDOKU_COL_NUM; i++) //checking the line
+        if(v[lin*SUDOKU_COL_NUM + i] == value)
             return false;
 
-    for(i=0;i<9;i++) //checking the col
-        if(v[col][i] == value)
+    for(i=0; i<SUDOKU_LIN_NUM; i++) //checking the col
+        if(v[i*SUDOKU_COL_NUM + col] == value)
             return false;
 
-    if(lin < 3 && col < 3) //first sub square
+    if(lin < 3 && col < 3) //first upper sub square
     {
         for(i=0;i<3;i++)
-            for(j=col;j<3;j++)
-                if(v[i][j] == value)
+            for(j=0;j<3;j++)
+                if(v[i*SUDOKU_COL_NUM + j] == value)
                     return false;
     }
-    else if(lin < 3 && col < 6) //second sub square
+    else if(lin < 3 && col < 6) //second upper sub square
     {
             for(i=0;i<3;i++)
-                for(j=col;j<6;j++)
-                    if(v[i][j] == value)
+                for(j=3;j<6;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
-    else if(lin < 3 && col < 9) //third sub square
+    else if(lin < 3 && col < 9) //third upper sub square
     {
             for(i=0;i<3;i++)
-                for(j=col;j<9;j++)
-                    if(v[i][j] == value)
+                for(j=6;j<9;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
-    if(lin < 6 && col < 3) //first sub square
+    else if(lin < 6 && col < 3) //first middle sub square
     {
         for(i=3;i<6;i++)
-            for(j=col;j<3;j++)
-                if(v[i][j] == value)
+            for(j=0;j<3;j++)
+                if(v[i*SUDOKU_COL_NUM + j] == value)
                     return false;
     }
-    else if(lin < 6 && col < 6) //second sub square
+    else if(lin < 6 && col < 6) //second middle sub square
     {
             for(i=3;i<6;i++)
-                for(j=col;j<6;j++)
-                    if(v[i][j] == value)
+                for(j=3;j<6;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
-    else if(lin < 6 && col < 9) //third sub square
+    else if(lin < 6 && col < 9) //third middle sub square
     {
             for(i=3;i<6;i++)
-                for(j=col;j<9;j++)
-                    if(v[i][j] == value)
+                for(j=6;j<9;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
-    if(lin < 9 && col < 3) //first sub square
+    else if(lin < 9 && col < 3) //first lower sub square
     {
         for(i=6;i<9;i++)
-            for(j=col;j<3;j++)
-                if(v[i][j] == value)
+            for(j=0;j<3;j++)
+                if(v[i*SUDOKU_COL_NUM + j] == value)
                     return false;
     }
-    else if(lin < 9 && col < 6) //second sub square
+    else if(lin < 9 && col < 6) //second lower sub square
     {
             for(i=6;i<9;i++)
-                for(j=col;j<6;j++)
-                    if(v[i][j] == value)
+                for(j=3;j<6;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
-    else if(lin < 9 && col < 9) //third sub square
+    else if(lin < 9 && col < 9) //third lower sub square
     {
             for(i=6;i<9;i++)
-                for(j=col;j<9;j++)
-                    if(v[i][j] == value)
+                for(j=6;j<9;j++)
+                    if(v[i*SUDOKU_COL_NUM + j] == value)
                         return false;
     }
 
     return true;
 }
 
-bool sudoku_solve(int v[][9], int lin, int col)
+bool sudoku_solve(int v[SUDOKU_SIZE], int pos) //Function that find the sudoku solution
 {
     int value, i, j;
 
-    for(i=lin;i<9;i++)
+    for(i = pos; i < SUDOKU_SIZE; i++)
     {
-        for(j=col;j<9;j++)
+        if(v[i] == 0)
         {
-            if(v[i][j] == 0)
+            for(value = 1; value < 10; value++)
             {
-                for(value = 1; value < 10; value++)
+                if(sudoku_check(v, value, i))
                 {
-                    if(sudoku_check(v, value, i, j))
-                    {
-                        v[i][j] = value;
-                    }
-                    else
-                    {
-                        continue;
-                    }
-
-                    if(sudoku_solve(v, i, j))
-                    {
-                        return true;
-                    }
+                    v[i] = value;
+                }
+                else
+                {
+                    continue;
                 }
 
-                return false;
+                if(sudoku_solve(v, i+1))
+                    return true;
+                v[i] = 0;
             }
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 int main()
 {
-    int s[9][9];
+    int s[SUDOKU_SIZE], i;
+    bool test = true;
 
-    sudoku_read(s);
-    printf("%d\n", sudoku_check(s, 1, 0, 2));
-    sudoku_solve(s, 0, 0);
-    sudoku_print(s);
+    sudoku_read(s); //Read the sudoku
+    if(sudoku_solve(s, 0)) //Find the solution
+    {
+        printf("Solution found!!!\n");
+        sudoku_print(s); //print solution
+    }
+    else
+    {
+        printf("Solution doesn't exist!!!\n");
+    }
 
     return 0;
 }
